425c0213a5aae784d07040212f80c585f7815fdc
hrithwik6986 <4ni17is028_a@nie.ac.in>
2020-12-23 11:05:32 +0530

    pwa option

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var cors = require('cors')
 // const cors = require('cors')
 // app.use(cors())
 const server = require('http').Server(app)
-var forceSSL = require('express-force-ssl');
+
 const io = require('socket.io')(server)
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
@@ -14,15 +14,11 @@ const peerServer = ExpressPeerServer(server, {
 const { v4: uuidV4 } = require('uuid')
 app.use(cors())
 app.use('/peerjs', peerServer);
-app.set('forceSSLOptions', {
-  enable301Redirects: false,
-  trustXFPHeader: false,
-  httpsPort: 443,
-});
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/',forceSSL, (req, res) => {
+app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
 
